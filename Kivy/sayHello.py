d6caef2199d4c6ff25bc362b3896cf0fe1b9e478
MiKiNGuilherme <guilhermebalopes@ufu.br>
2022-02-21 13:46:22 -0300

    Create sayHello.py

---
+++ Kivy/sayHello.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+
+class SayHello(App):
+    """ 
+    Classe App
+    """
+    def build(self):
+        self.window = GridLayout()
