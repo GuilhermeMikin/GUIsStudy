3adac2215a56367f62c09afc58c98428f303ebdf
MiKiNGuilherme <guilhermebalopes@ufu.br>
2022-03-01 11:38:30 -0300

    updt

--- Kivy/kivy_bolinha/imports.py
+++ Kivy/kivy_bolinha/imports.py
@@ -9,3 +9,6 @@ from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.vector import Vector
